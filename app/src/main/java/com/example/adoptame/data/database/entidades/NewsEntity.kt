f70e828580ae4967199b39fe7cd80a9fd3de4384
LAPTOP-GURCE7P3\torre <torres.bayron@gmail.com>
2022-02-07 18:02:11 -0500

    Se agrega Retrofit para manejo de APIs

---
+++ app/src/main/java/com/example/adoptame/data/database/entidades/NewsEntity.kt
@@ -0,0 +1,27 @@
+package com.example.adoptame.database.entidades
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Entity(tableName = "news")
+@Serializable
+data class NewsEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val author: String?,
+    val title: String?,
+    val desc: String?,
+    var img: String?
+) {
+    init {
+        if (this.img == null) {
+            this.img =
+                "https://isabelpaz.com/wp-content/themes/nucleare-pro/images/no-image-box.png"
+        }
+    }
+}
+
+
+
+
